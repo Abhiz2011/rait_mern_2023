b801d2aa2f8a8e93868065ce1f6ebf9c975ea432
AtharvAwle <atharvawle0156@gmail.com>
2024-01-11 23:04:00 +0530

    created a react app and installed all the dependencies[express,mongoose,nodemon,socket.io,bcrypt,.env]

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+// app creation hora via express
+const app = express()
+require("dotenv").config();
